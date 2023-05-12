0ac35979419f664774eebf431f44b0819e08927c
sarpdora23 <sarpdorayonden@gmail.com>
2023-05-12 10:52:39 +0300

    TargetInit nasıl yok oldu amk

---
+++ src/main/java/com/example/sinamekihunter/Controllers/TargetInit.java
@@ -0,0 +1,14 @@
+package com.example.sinamekihunter.Controllers;
+
+import javafx.fxml.FXML;
+
+public class TargetInit implements ControllersParent{
+    @Override
+    public void InitController() {
+
+    }
+    @FXML
+    protected void onStartAttack(){
+
+    }
+}
